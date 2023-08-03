76800e9cd4abc30cbf36753f88b35ef153f62ddc
olamission Ayodele <Oayodele@hotmail.co.uk>
2023-08-03 10:52:02 +0000

    first commit

---
+++ send/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Send // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
